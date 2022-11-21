2aba767b61d39e27b39afe57aa934b5cd7707456
Moulya K S <117506087+Moulya53@users.noreply.github.com>
2022-11-21 10:42:27 +0530

    QuadraticEquation.java

--- QuadraticEquation.java
+++ QuadraticEquation.java
@@ -8,23 +8,30 @@ System.out.print("Enter the value of a: ");
 double a = input.nextDouble();  
 System.out.print("Enter the value of b: ");  
 double b = input.nextDouble();  
-System.out.print("Enter the value of c: ")  
+System.out.print("Enter the value of c: ");
+  //Updated with Sytax errors
 double c = input.nextDouble();  
 double d= b * b - 4.0 * a * c;  
 if (d> 0.0)   
 {  
+  System.out.println("Roots are real and distinct");
 double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);  
 double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);  
 System.out.println("The roots are " + r1 + " and " + r2)  
 }   
 else if (d == 0.0)   
-{  
+{
+   System.out.println("Roots are real and equal");
 double r1 = -b / (2.0 * a);  
 System.out.println("The root is " + r1);  
 }   
 else   
-{  
-System.out.println("Roots are not real.")
+{ 
+System.out.println("Roots are not real.");
+  double r1=-b/(2.0*a);
+  double r2=Math.sqrt(d)/(2.0*a);
+  System.out.println("Root1= " +r1"+i+"+r2);
+   System.out.println("Root1= " +r1"-i+"+r2);
 }  
 }  
 }  
